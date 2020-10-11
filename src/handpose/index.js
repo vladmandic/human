e2bd9dbc345298f1de2c961aef774d7a25d87953
Vladimir Mandic <mandic00@live.com>
2020-10-11 19:22:43 -0400

    initial public commit

---
+++ src/handpose/index.js
@@ -0,0 +1,93 @@
+const tf = require('@tensorflow/tfjs');
+const hand = require('./hand');
+const keypoints = require('./keypoints');
+const pipe = require('./pipeline');
+
+// Load the bounding box detector model.
+async function loadHandDetectorModel(url) {
+  return tf.loadGraphModel(url, { fromTFHub: url.includes('tfhub.dev') });
+}
+
+// Load the mesh detector model.
+async function loadHandPoseModel(url) {
+  return tf.loadGraphModel(url, { fromTFHub: url.includes('tfhub.dev') });
+}
+
+// In single shot detector pipelines, the output space is discretized into a set
+// of bounding boxes, each of which is assigned a score during prediction. The
+// anchors define the coordinates of these boxes.
+async function loadAnchors(url) {
+  return tf.util
+    .fetch(url)
+    .then((d) => d.json());
+}
+
+/**
+ * Load handpose.
+ *
+ * @param config A configuration object with the following properties:
+ * - `maxContinuousChecks` How many frames to go without running the bounding
+ * box detector. Defaults to infinity. Set to a lower value if you want a safety
+ * net in case the mesh detector produces consistently flawed predictions.
+ * - `detectionConfidence` Threshold for discarding a prediction. Defaults to
+ * 0.8.
+ * - `iouThreshold` A float representing the threshold for deciding whether
+ * boxes overlap too much in non-maximum suppression. Must be between [0, 1].
+ * Defaults to 0.3.
+ * - `scoreThreshold` A threshold for deciding when to remove boxes based
+ * on score in non-maximum suppression. Defaults to 0.75.
+ */
+async function load(config) {
+  const [ANCHORS, handDetectorModel, handPoseModel] = await Promise.all([
+    loadAnchors(config.detector.anchors),
+    loadHandDetectorModel(config.detector.modelPath),
+    loadHandPoseModel(config.skeleton.modelPath),
+  ]);
+  const detector = new hand.HandDetector(handDetectorModel, config.inputSize, config.inputSize, ANCHORS, config.iouThreshold, config.scoreThreshold);
+  const pipeline = new pipe.HandPipeline(detector, handPoseModel, config.inputSize, config.inputSize, config.skipFrames, config.minConfidence);
+  // eslint-disable-next-line no-use-before-define
+  const handpose = new HandPose(pipeline);
+  return handpose;
+}
+exports.load = load;
+
+class HandPose {
+  constructor(pipeline) {
+    this.pipeline = pipeline;
+  }
+
+  static getAnnotations() {
+    return keypoints.MESH_ANNOTATIONS;
+  }
+
+  /**
+     * Finds hands in the input image.
+     *
+     * @param input The image to classify. Can be a tensor, DOM element image,
+     * video, or canvas.
+     * @param flipHorizontal Whether to flip the hand keypoints horizontally.
+     * Should be true for videos that are flipped by default (e.g. webcams).
+     */
+  async estimateHands(input, config) {
+    const image = tf.tidy(() => {
+      if (!(input instanceof tf.Tensor)) {
+        input = tf.browser.fromPixels(input);
+      }
+      return input.toFloat().expandDims(0);
+    });
+    const prediction = await this.pipeline.estimateHand(image, config);
+    image.dispose();
+    if (!prediction) return [];
+    const annotations = {};
+    for (const key of Object.keys(keypoints.MESH_ANNOTATIONS)) {
+      annotations[key] = keypoints.MESH_ANNOTATIONS[key].map((index) => prediction.landmarks[index]);
+    }
+    return [{
+      confidence: prediction.confidence || 0,
+      box: prediction.box ? [prediction.box.topLeft[0], prediction.box.topLeft[1], prediction.box.bottomRight[0] - prediction.box.topLeft[0], prediction.box.bottomRight[1] - prediction.box.topLeft[1]] : 0,
+      landmarks: prediction.landmarks,
+      annotations,
+    }];
+  }
+}
+exports.HandPose = HandPose;
---
+++ src/index.js
@@ -0,0 +1,81 @@
+const facemesh = require('./facemesh/index.js');
+const ssrnet = require('./ssrnet/index.js');
+const posenet = require('./posenet/index.js');
+const handpose = require('./handpose/index.js');
+// const image = require('./image.js');
+// const triangulation = require('./triangulation.js').default;
+const defaults = require('./config.js').default;
+
+const models = {
+  facemesh: null,
+  blazeface: null,
+  ssrnet: null,
+  iris: null,
+};
+
+function mergeDeep(...objects) {
+  const isObject = (obj) => obj && typeof obj === 'object';
+  return objects.reduce((prev, obj) => {
+    Object.keys(obj).forEach((key) => {
+      const pVal = prev[key];
+      const oVal = obj[key];
+      if (Array.isArray(pVal) && Array.isArray(oVal)) {
+        prev[key] = pVal.concat(...oVal);
+      } else if (isObject(pVal) && isObject(oVal)) {
+        prev[key] = mergeDeep(pVal, oVal);
+      } else {
+        prev[key] = oVal;
+      }
+    });
+    return prev;
+  }, {});
+}
+
+async function detect(input, userConfig) {
+  const config = mergeDeep(defaults, userConfig);
+
+  // run posenet
+  let poseRes = [];
+  if (config.body.enabled) {
+    if (!models.posenet) models.posenet = await posenet.load(config.body);
+    poseRes = await models.posenet.estimateMultiplePoses(input, config.body);
+  }
+
+  // run handpose
+  let handRes = [];
+  if (config.hand.enabled) {
+    if (!models.handpose) models.handpose = await handpose.load(config.hand);
+    handRes = await models.handpose.estimateHands(input, config.hand);
+  }
+
+  // run facemesh, includes blazeface and iris
+  const faceRes = [];
+  if (config.face.enabled) {
+    if (!models.facemesh) models.facemesh = await facemesh.load(config.face);
+    const faces = await models.facemesh.estimateFaces(input, config.face);
+    for (const face of faces) {
+      // run ssr-net age & gender, inherits face from blazeface
+      const ssrdata = (config.face.age.enabled || config.face.gender.enabled) ? await ssrnet.predict(face.image, config) : {};
+      // iris: array[ bottom, left, top, right, center ]
+      const iris = (face.annotations.leftEyeIris && face.annotations.rightEyeIris)
+        ? Math.max(face.annotations.leftEyeIris[3][0] - face.annotations.leftEyeIris[1][0], face.annotations.rightEyeIris[3][0] - face.annotations.rightEyeIris[1][0])
+        : 0;
+      faceRes.push({
+        confidence: face.confidence,
+        box: face.box,
+        mesh: face.mesh,
+        annotations: face.annotations,
+        age: ssrdata.age,
+        gender: ssrdata.gender,
+        iris: (iris !== 0) ? Math.trunc(100 * 11.7 / iris) / 100 : 0,
+      });
+    }
+  }
+
+  // combine results
+  return { face: faceRes, body: poseRes, hand: handRes };
+}
+
+exports.detect = detect;
+exports.defaults = defaults;
+exports.models = models;

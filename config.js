792c1064b0d3b25442c09c59d946028eba3cf5a4
Vladimir Mandic <mandic00@live.com>
2020-11-01 13:10:22 -0500

    changed defaults

--- config.js
+++ config.js
@@ -4,16 +4,18 @@
 export default {
   backend: 'webgl',          // select tfjs backend to use
   console: true,             // enable debugging output to console
-  profile: true,             // enable tfjs profiling
+  profile: false,            // enable tfjs profiling
                              // this has significant performance impact, only enable for debugging purposes
                              // currently only implemented for age,gender,emotion models
-  deallocate: true,          // aggresively deallocate gpu memory after each usage
+  deallocate: false,         // aggresively deallocate gpu memory after each usage
                              // only valid for webgl backend and only during first call, cannot be changed unless library is reloaded
                              // this has significant performance impact, only enable on low-memory devices
   scoped: false,             // enable scoped runs
                              // some models *may* have memory leaks, this wrapps everything in a local scope at a cost of performance
                              // typically not needed
   videoOptimized: true,      // perform additional optimizations when input is video, must be disabled for images
+                             // basically this skips object box boundary detection for every n frames
+                             // while maintaining in-box detection since objects cannot move that fast
   filter: {
     enabled: true,           // enable image pre-processing filters
     width: 0,                // resize input width

2a041305e91545c4483f56ed77ebd6d0f755f038
Vladimir Mandic <mandic00@live.com>
2021-01-13 09:35:31 -0500

    added humangl custom backend

--- src/tfjs/backend.js
+++ src/tfjs/backend.js
@@ -1,9 +1,11 @@
+import { log } from '../log.js';
 import * as tf from '../../dist/tfjs.esm.js';
 
 export const config = {
   name: 'humangl',
   priority: 99,
   canvas: null,
+  gl: null,
   width: 1024,
   height: 1024,
   webGLattr: { // https://www.khronos.org/registry/webgl/specs/latest/1.0/#5.2
@@ -20,21 +22,51 @@ export const config = {
 
 export function register() {
   if (!tf.findBackend(config.name)) {
-    // @ts-ignore
-    config.canvas = (typeof OffscreenCanvas !== 'undefined') ? new OffscreenCanvas(config.width, config.height) : document.createElement('canvas');
-    // @ts-ignore
-    const gl = config.canvas.getContext('webgl2', config.webGLattr);
-    tf.setWebGLContext(2, gl);
-    const ctx = new tf.GPGPUContext(gl);
-    tf.registerBackend(config.name, () => new tf.MathBackendWebGL(ctx), config.priority);
-    const kernels = tf.getKernelsForBackend('webgl');
-    kernels.forEach((kernelConfig) => {
-      const newKernelConfig = { ...kernelConfig, backendName: config.name };
-      tf.registerKernel(newKernelConfig);
-    });
-    tf.ENV.set('WEBGL_VERSION', 2);
-    tf.ENV.set('WEBGL_MAX_TEXTURE_SIZE', gl.getParameter(gl.MAX_TEXTURE_SIZE));
-    tf.ENV.set('WEBGL_FORCE_F16_TEXTURES', true);
-    tf.ENV.set('WEBGL_PACK_DEPTHWISECONV', true);
+    log('backend registration:', config.name);
+    try {
+      config.canvas = (typeof OffscreenCanvas !== 'undefined') ? new OffscreenCanvas(config.width, config.height) : document.createElement('canvas');
+    } catch (err) {
+      log('error: cannot create canvas:', err);
+      return;
+    }
+    try {
+      config.gl = config.canvas.getContext('webgl2', config.webGLattr);
+    } catch (err) {
+      log('error: cannot get WebGL2 context:', err);
+      return;
+    }
+    try {
+      tf.setWebGLContext(2, config.gl);
+    } catch (err) {
+      log('error: cannot set WebGL2 context:', err);
+      return;
+    }
+    try {
+      const ctx = new tf.GPGPUContext(config.gl);
+      tf.registerBackend(config.name, () => new tf.MathBackendWebGL(ctx), config.priority);
+    } catch (err) {
+      log('error: cannot register WebGL backend:', err);
+      return;
+    }
+    try {
+      const kernels = tf.getKernelsForBackend('webgl');
+      kernels.forEach((kernelConfig) => {
+        const newKernelConfig = { ...kernelConfig, backendName: config.name };
+        tf.registerKernel(newKernelConfig);
+      });
+    } catch (err) {
+      log('error: cannot update WebGL backend registration:', err);
+      return;
+    }
+    try {
+      tf.ENV.set('WEBGL_VERSION', 2);
+      tf.ENV.set('WEBGL_MAX_TEXTURE_SIZE', config.gl.getParameter(config.gl.MAX_TEXTURE_SIZE));
+      tf.ENV.set('WEBGL_FORCE_F16_TEXTURES', true);
+      tf.ENV.set('WEBGL_PACK_DEPTHWISECONV', true);
+    } catch (err) {
+      log('error: cannot set WebGL backend flags:', err);
+      return;
+    }
+    log('backend registered:', config.name);
   }
 }

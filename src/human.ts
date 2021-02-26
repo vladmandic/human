b7e0674abefd6a0589d36ff67f4d0fe68141f446
Vladimir Mandic <mandic00@live.com>
2021-02-26 09:04:08 -0500

    rebuild

--- src/human.ts
+++ src/human.ts
@@ -29,13 +29,9 @@ function mergeDeep(...objects) {
     Object.keys(obj || {}).forEach((key) => {
       const pVal = prev[key];
       const oVal = obj[key];
-      if (Array.isArray(pVal) && Array.isArray(oVal)) {
-        prev[key] = pVal.concat(...oVal);
-      } else if (isObject(pVal) && isObject(oVal)) {
-        prev[key] = mergeDeep(pVal, oVal);
-      } else {
-        prev[key] = oVal;
-      }
+      if (Array.isArray(pVal) && Array.isArray(oVal)) prev[key] = pVal.concat(...oVal);
+      else if (isObject(pVal) && isObject(oVal)) prev[key] = mergeDeep(pVal, oVal);
+      else prev[key] = oVal;
     });
     return prev;
   }, {});

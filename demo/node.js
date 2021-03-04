75161c912700e0cda864b029d165bf6af05bf665
Vladimir Mandic <mandic00@live.com>
2021-03-04 10:33:08 -0500

    implement blazepose and update demos

--- demo/node.js
+++ demo/node.js
@@ -14,16 +14,19 @@ const myConfig = {
   videoOptimized: false,
   async: false,
   face: {
-    // detector: { modelPath: 'file://models/faceboxes.json' },
-    detector: { modelPath: 'file://models/blazeface-back.json' }, // cannot use blazeface in nodejs due to missing required kernel function in tfjs-node
-    mesh: { modelPath: 'file://models/facemesh.json' },
-    iris: { modelPath: 'file://models/iris.json' },
-    age: { modelPath: 'file://models/age-ssrnet-imdb.json' },
-    gender: { modelPath: 'file://models/gender.json' },
-    emotion: { modelPath: 'file://models/emotion-large.json' },
+    enabled: true,
+    detector: { modelPath: 'file://models/faceboxes.json', enabled: true, minConfidence: 0.5 },
+    // detector: { modelPath: 'file://models/blazeface-back.json', enabled: false }, // cannot use blazeface in nodejs due to missing required kernel function in tfjs-node
+    mesh: { modelPath: 'file://models/facemesh.json', enabled: false }, // depends on blazeface detector
+    iris: { modelPath: 'file://models/iris.json', enabled: true },
+    age: { modelPath: 'file://models/age-ssrnet-imdb.json', enabled: true },
+    gender: { modelPath: 'file://models/gender.json', enabled: true },
+    emotion: { modelPath: 'file://models/emotion.json', enabled: true },
   },
-  body: { modelPath: 'file://models/posenet.json' },
+  // body: { modelPath: 'file://models/blazepose.json', modelType: 'blazepose', inputSize: 256, enabled: true },
+  body: { modelPath: 'file://models/posenet.json', modelType: 'posenet', inputSize: 257, enabled: true },
   hand: {
+    enabled: true,
     detector: { modelPath: 'file://models/handdetect.json' },
     skeleton: { modelPath: 'file://models/handskeleton.json' },
   },
@@ -35,7 +38,14 @@ async function init() {
   // create instance of human
   human = new Human(myConfig);
   // pre-load models
+  log.info('Human:', human.version);
+  log.info('Active Configuration', human.config);
+  log.info('TFJS Version:', human.tf.version_core, 'Backend:', tf.getBackend());
+  log.info('TFJS Flags:', human.tf.env().features);
   await human.load();
+  const loaded = Object.keys(human.models).filter((a) => human.models[a]);
+  log.info('Loaded:', loaded);
+  log.info('Memory state:', human.tf.engine().memory());
 }
 
 async function detect(input) {
@@ -74,6 +84,7 @@ async function test() {
 
 async function main() {
   log.info('NodeJS:', process.version);
+  log.info('Current folder:', process.env.PWD);
   await init();
   if (process.argv.length !== 3) {
     log.warn('Parameters: <input image> missing');

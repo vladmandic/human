55b12a629c0bcd446d8cc5ded3c88ce6098c7931
Vladimir Mandic <mandic00@live.com>
2021-03-17 18:23:19 -0400

    type definitions

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,121 @@
 /* eslint-disable indent */
 /* eslint-disable no-multi-spaces */
 
-export default {
+/**
+ * Configuration interface definition for **Human** library
+ *
+ * Contains all configurable parameters
+ */
+export interface Config {
+  backend: String,
+  wasmPath: String,
+  debug: Boolean,
+  async: Boolean,
+  profile: Boolean,
+  deallocate: Boolean,
+  scoped: Boolean,
+  videoOptimized: Boolean,
+  warmup: String,
+  filter: {
+    enabled: Boolean,
+    width: Number,
+    height: Number,
+    return: Boolean,
+    brightness: Number,
+    contrast: Number,
+    sharpness: Number,
+    blur: Number
+    saturation: Number,
+    hue: Number,
+    negative: Boolean,
+    sepia: Boolean,
+    vintage: Boolean,
+    kodachrome: Boolean,
+    technicolor: Boolean,
+    polaroid: Boolean,
+    pixelate: Number,
+  },
+  gesture: {
+    enabled: Boolean,
+  },
+  face: {
+    enabled: Boolean,
+    detector: {
+      modelPath: String,
+      rotation: Boolean,
+      maxFaces: Number,
+      skipFrames: Number,
+      skipInitial: Boolean,
+      minConfidence: Number,
+      iouThreshold: Number,
+      scoreThreshold: Number,
+      return: Boolean,
+    },
+    mesh: {
+      enabled: Boolean,
+      modelPath: String,
+    },
+    iris: {
+      enabled: Boolean,
+      modelPath: String,
+    },
+    age: {
+      enabled: Boolean,
+      modelPath: String,
+      skipFrames: Number,
+    },
+    gender: {
+      enabled: Boolean,
+      minConfidence: Number,
+      modelPath: String,
+      skipFrames: Number,
+    },
+    emotion: {
+      enabled: Boolean,
+      minConfidence: Number,
+      skipFrames: Number,
+      modelPath: String,
+    },
+    embedding: {
+      enabled: Boolean,
+      modelPath: String,
+    },
+  },
+  body: {
+    enabled: Boolean,
+    modelPath: String,
+    maxDetections: Number,
+    scoreThreshold: Number,
+    nmsRadius: Number,
+  },
+  hand: {
+    enabled: Boolean,
+    rotation: Boolean,
+    skipFrames: Number,
+    skipInitial: Boolean,
+    minConfidence: Number,
+    iouThreshold: Number,
+    scoreThreshold: Number,
+    maxHands: Number,
+    landmarks: Boolean,
+    detector: {
+      modelPath: String,
+    },
+    skeleton: {
+      modelPath: String,
+    },
+  },
+  object: {
+    enabled: Boolean,
+    modelPath: String,
+    minConfidence: Number,
+    iouThreshold: Number,
+    maxResults: Number,
+    skipFrames: Number,
+  },
+}
+
+const config: Config = {
   backend: 'webgl',          // select tfjs backend to use
                              // can be 'webgl', 'wasm', 'cpu', or 'humangl' which is a custom version of webgl
                              // leave as empty string to continue using default backend
@@ -177,3 +291,4 @@ export default {
     skipFrames: 13,          // how many frames to go without re-running the detector
   },
 };
+export { config as defaults };

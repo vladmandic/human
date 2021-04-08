b5c77fd14965b198cdca4a8385f922de9f5e321f
Vladimir Mandic <mandic00@live.com>
2021-04-08 12:10:15 -0400

    update

--- src/tfjs/tf-browser.ts
+++ src/tfjs/tf-browser.ts
@@ -1,13 +1,12 @@
 // wrapper to load tfjs in a single place so version can be changed quickly
 
 // simplified
-// { modules: 1061, moduleBytes: 3772720, outputBytes: 1531035 }
-
+// { modules: 1250, moduleBytes: 4013323, imports: 7, importBytes: 2255, outputBytes: 2991826, outputFiles: 'dist/tfjs.esm.js' }
 // export * from '@tensorflow/tfjs/dist/index.js';
 // export * from '@tensorflow/tfjs-backend-wasm';
 
 // modular
-// { modules: 1064, moduleBytes: 3793219, outputBytes: 1535600 }
+// { modules: 1253, moduleBytes: 4029357, imports: 7, importBytes: 2285, outputBytes: 2998298, outputFiles: 'dist/tfjs.esm.js' }
 
 // get versions of all packages.
 import * as packageBundle from '@tensorflow/tfjs/package.json';
@@ -20,7 +19,7 @@ import { version_cpu } from '@tensorflow/tfjs-backend-cpu/dist/index.js';
 import { version_webgl } from '@tensorflow/tfjs-backend-webgl/dist/index.js';
 import { version_wasm } from '@tensorflow/tfjs-backend-wasm/dist/index.js';
 
-// export all
+// export all - compiled
 export * from '@tensorflow/tfjs-core/dist/index.js';
 export * from '@tensorflow/tfjs-layers/dist/index.js';
 export * from '@tensorflow/tfjs-converter/dist/index.js';
@@ -29,14 +28,26 @@ export * from '@tensorflow/tfjs-backend-cpu/dist/index.js';
 export * from '@tensorflow/tfjs-backend-webgl/dist/index.js';
 export * from '@tensorflow/tfjs-backend-wasm/dist/index.js';
 
+// export all - sources
+/*
+export * from '@tensorflow/tfjs-core/src/index';
+export * from '@tensorflow/tfjs-layers/src/index';
+export * from '@tensorflow/tfjs-converter/src/index';
+export * as data from '@tensorflow/tfjs-data/src/index';
+export * from '@tensorflow/tfjs-backend-cpu/src/index';
+export * from '@tensorflow/tfjs-backend-webgl/src/index';
+export * from '@tensorflow/tfjs-backend-wasm/src/index';
+*/
+
 // export versions
 export const version = {
-  tfjs: packageBundle.version,
-  'tfjs-core': packageCore.version,
-  'tfjs-data': packageData.version,
-  'tfjs-layers': packageLayers.version,
-  'tfjs-converter': packageConverter.version,
-  'tfjs-backend-cpu': version_cpu,
-  'tfjs-backend-webgl': version_webgl,
-  'tfjs-backend-wasm': version_wasm,
+  tfjs: packageBundle?.version || undefined,
+  'tfjs-core': packageCore?.version || undefined,
+  'tfjs-data': packageData?.version || undefined,
+  'tfjs-layers': packageLayers?.version || undefined,
+  'tfjs-converter': packageConverter?.version || undefined,
+  'tfjs-backend-cpu': version_cpu || undefined,
+  'tfjs-backend-webgl': version_webgl || undefined,
+  'tfjs-backend-wasm': version_wasm || undefined,
 };
+// export const version = {};

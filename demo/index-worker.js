613a86fbf4319a0004ea258abaeaf5ebad8459e2
Vladimir Mandic <mandic00@live.com>
2021-06-05 15:10:28 -0400

    update

--- demo/index-worker.js
+++ demo/index-worker.js
@@ -29,11 +29,13 @@ onmessage = async (msg) => {
   }
 
   if (result.canvas) { // convert canvas to imageData and send it by reference
-    const ctx = result.canvas.getContext('2d');
-    const img = ctx?.getImageData(0, 0, result.canvas.width, result.canvas.height);
+    const canvas = new OffscreenCanvas(result.canvas.width, result.canvas.height);
+    const ctx = canvas.getContext('2d');
+    if (ctx) ctx.drawImage(result.canvas, 0, 0);
+    const img = ctx ? ctx.getImageData(0, 0, result.canvas.width, result.canvas.height) : null;
     result.canvas = null; // must strip original canvas from return value as it cannot be transfered from worker thread
     // @ts-ignore tslint wrong type matching for worker
-    if (img) postMessage({ result, image: img.data.buffer, width: msg.data.width, height: msg.data.height }, [img?.data.buffer]);
+    if (img) postMessage({ result, image: img.data.buffer, width: msg.data.width, height: msg.data.height }, [img.data.buffer]);
     // @ts-ignore tslint wrong type matching for worker
     else postMessage({ result });
   } else {

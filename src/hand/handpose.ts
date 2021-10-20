7a05cf3743bea22303352b2fb906df4623071bfb
Vladimir Mandic <mandic00@live.com>
2021-10-20 09:10:57 -0400

    update handtrack boxes and refactor handpose

--- src/hand/handpose.ts
+++ src/hand/handpose.ts
@@ -6,9 +6,9 @@
 
 import { log, join } from '../util/util';
 import * as tf from '../../dist/tfjs.esm.js';
-import * as handdetector from './handdetector';
-import * as handpipeline from './handpipeline';
-import * as fingerPose from '../hand/fingerpose';
+import * as handdetector from './handposedetector';
+import * as handpipeline from './handposepipeline';
+import * as fingerPose from './fingerpose';
 import type { HandResult, Box, Point } from '../result';
 import type { Tensor, GraphModel } from '../tfjs/types';
 import type { Config } from '../config';

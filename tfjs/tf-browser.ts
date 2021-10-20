7f87d2633aff41150bf0f8615536741ef8d459ca
Vladimir Mandic <mandic00@live.com>
2021-10-20 17:49:00 -0400

    add human.custom.esm using custom tfjs build

--- tfjs/tf-browser.ts
+++ tfjs/tf-browser.ts
@@ -3,29 +3,6 @@
  * @external
  */
 
-// export all from sources
-// requires treeShaking:ignore-annotations due to tfjs misconfiguration
-/*
-export * from '@tensorflow/tfjs-core/src/index';
-export * from '@tensorflow/tfjs-layers/src/index';
-export * from '@tensorflow/tfjs-converter/src/index';
-export * as data from '@tensorflow/tfjs-data/src/index';
-export * from '@tensorflow/tfjs-backend-cpu/src/index';
-export * from '@tensorflow/tfjs-backend-webgl/src/index';
-export * from '@tensorflow/tfjs-backend-wasm/src/index';
-*/
-
-// export all from build individual packages
-/*
-export * from '@tensorflow/tfjs-core/dist/index.js';
-export * from '@tensorflow/tfjs-layers/dist/index.js';
-export * from '@tensorflow/tfjs-converter/dist/index.js';
-export * as data from '@tensorflow/tfjs-data/dist/index.js';
-export * from '@tensorflow/tfjs-backend-cpu/dist/index.js';
-export * from '@tensorflow/tfjs-backend-webgl/dist/index.js';
-export * from '@tensorflow/tfjs-backend-wasm/dist/index.js';
-*/
-
 // export all from build bundle
 export * from '@tensorflow/tfjs/dist/index.js';
 export * from '@tensorflow/tfjs-backend-webgl/dist/index.js';

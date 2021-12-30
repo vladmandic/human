ecb5bd190809b7e27e5bd93d8da0e3d25418a7be
Vladimir Mandic <mandic00@live.com>
2021-12-30 12:14:09 -0500

    update hand annotations

--- dist/human.node-wasm.js
+++ dist/human.node-wasm.js
@@ -10042,12 +10042,13 @@ var cache4 = {
   hands: []
 };
 var fingerMap = {
-  thumb: [0, 1, 2, 3, 4],
-  index: [0, 5, 6, 7, 8],
-  middle: [0, 9, 10, 11, 12],
-  ring: [0, 13, 14, 15, 16],
-  pinky: [0, 17, 18, 19, 20],
-  palm: [0]
+  thumb: [1, 2, 3, 4],
+  index: [5, 6, 7, 8],
+  middle: [9, 10, 11, 12],
+  ring: [13, 14, 15, 16],
+  pinky: [17, 18, 19, 20],
+  base: [0],
+  palm: [0, 17, 13, 9, 5, 1, 0]
 };
 async function loadDetect2(config3) {
   var _a, _b;

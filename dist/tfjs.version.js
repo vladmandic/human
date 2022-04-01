5214eaaf6a80510998441fbd904b7c13c8828053
Vladimir Mandic <mandic00@live.com>
2022-04-01 09:13:32 -0400

    update

--- dist/tfjs.version.js
+++ dist/tfjs.version.js
@@ -4,41 +4,4 @@
   author: <https://github.com/vladmandic>'
 */
 
-// node_modules/.pnpm/@tensorflow+tfjs@3.15.0_seedrandom@3.0.5/node_modules/@tensorflow/tfjs/package.json
-var version = "3.15.0";
-
-// node_modules/.pnpm/@tensorflow+tfjs-core@3.15.0/node_modules/@tensorflow/tfjs-core/package.json
-var version2 = "3.15.0";
-
-// node_modules/.pnpm/@tensorflow+tfjs-data@3.15.0_ec5705835d0d8c568be975e47d5966ee/node_modules/@tensorflow/tfjs-data/package.json
-var version3 = "3.15.0";
-
-// node_modules/.pnpm/@tensorflow+tfjs-layers@3.15.0_@tensorflow+tfjs-core@3.15.0/node_modules/@tensorflow/tfjs-layers/package.json
-var version4 = "3.15.0";
-
-// node_modules/.pnpm/@tensorflow+tfjs-converter@3.15.0_@tensorflow+tfjs-core@3.15.0/node_modules/@tensorflow/tfjs-converter/package.json
-var version5 = "3.15.0";
-
-// node_modules/.pnpm/@tensorflow+tfjs-backend-cpu@3.15.0_@tensorflow+tfjs-core@3.15.0/node_modules/@tensorflow/tfjs-backend-cpu/package.json
-var version6 = "3.15.0";
-
-// node_modules/.pnpm/@tensorflow+tfjs-backend-webgl@3.15.0_@tensorflow+tfjs-core@3.15.0/node_modules/@tensorflow/tfjs-backend-webgl/package.json
-var version7 = "3.15.0";
-
-// node_modules/.pnpm/@tensorflow+tfjs-backend-wasm@3.15.0_@tensorflow+tfjs-core@3.15.0/node_modules/@tensorflow/tfjs-backend-wasm/package.json
-var version8 = "3.15.0";
-
-// tfjs/tf-version.ts
-var version9 = {
-  tfjs: version,
-  "tfjs-core": version2,
-  "tfjs-data": version3,
-  "tfjs-layers": version4,
-  "tfjs-converter": version5,
-  "tfjs-backend-cpu": version6,
-  "tfjs-backend-webgl": version7,
-  "tfjs-backend-wasm": version8
-};
-export {
-  version9 as version
-};
+var e="3.15.0";var s="3.15.0";var t="3.15.0";var r="3.15.0";var l="3.15.0";var i="3.15.0";var a="3.15.0";var n="3.15.0";var G={tfjs:e,"tfjs-core":s,"tfjs-data":t,"tfjs-layers":r,"tfjs-converter":l,"tfjs-backend-cpu":i,"tfjs-backend-webgl":a,"tfjs-backend-wasm":n};export{G as version};

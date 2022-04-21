8cadcb671593569c589c44230b94f20eb320efc5
Vladimir Mandic <mandic00@live.com>
2022-04-21 09:39:40 -0400

    support 4k input

--- dist/tfjs.version.js
+++ dist/tfjs.version.js
@@ -4,37 +4,4 @@
   author: <https://github.com/vladmandic>'
 */
 
-// node_modules/.pnpm/@tensorflow+tfjs@3.16.0_seedrandom@3.0.5/node_modules/@tensorflow/tfjs/package.json
-var version = "3.16.0";
-
-// node_modules/.pnpm/@tensorflow+tfjs-core@3.16.0/node_modules/@tensorflow/tfjs-core/package.json
-var version2 = "3.16.0";
-
-// node_modules/.pnpm/@tensorflow+tfjs-data@3.16.0_2aa468dee376a890b79182404d4ab8c3/node_modules/@tensorflow/tfjs-data/package.json
-var version3 = "3.16.0";
-
-// node_modules/.pnpm/@tensorflow+tfjs-layers@3.16.0_@tensorflow+tfjs-core@3.16.0/node_modules/@tensorflow/tfjs-layers/package.json
-var version4 = "3.16.0";
-
-// node_modules/.pnpm/@tensorflow+tfjs-converter@3.16.0_@tensorflow+tfjs-core@3.16.0/node_modules/@tensorflow/tfjs-converter/package.json
-var version5 = "3.16.0";
-
-// node_modules/.pnpm/@tensorflow+tfjs-backend-webgl@3.16.0_@tensorflow+tfjs-core@3.16.0/node_modules/@tensorflow/tfjs-backend-webgl/package.json
-var version6 = "3.16.0";
-
-// node_modules/.pnpm/@tensorflow+tfjs-backend-wasm@3.16.0_@tensorflow+tfjs-core@3.16.0/node_modules/@tensorflow/tfjs-backend-wasm/package.json
-var version7 = "3.16.0";
-
-// tfjs/tf-version.ts
-var version8 = {
-  tfjs: version,
-  "tfjs-core": version2,
-  "tfjs-data": version3,
-  "tfjs-layers": version4,
-  "tfjs-converter": version5,
-  "tfjs-backend-webgl": version6,
-  "tfjs-backend-wasm": version7
-};
-export {
-  version8 as version
-};
+var e="3.16.0";var s="3.16.0";var t="3.16.0";var r="3.16.0";var l="3.16.0";var i="3.16.0";var a="3.16.0";var V={tfjs:e,"tfjs-core":s,"tfjs-data":t,"tfjs-layers":r,"tfjs-converter":l,"tfjs-backend-webgl":i,"tfjs-backend-wasm":a};export{V as version};

c625892e18ba7f957ee227ce82f3ddbd2bdc1346
Vladimir Mandic <mandic00@live.com>
2022-07-16 09:08:58 -0400

    swtich to release version of tfjs

--- tfjs/tf-custom.ts
+++ tfjs/tf-custom.ts
@@ -1,4 +1,24 @@
 /** Creates tfjs bundle used by Human browser build target
  * @external
  */
-export * from '@vladmandic/tfjs';
+// export * from '@vladmandic/tfjs';
+
+/**
+ * Creates tfjs bundle used by Human browser build target
+ * @external
+ */
+
+// export all from build bundle
+export * from '@tensorflow/tfjs/dist/index.js';
+export * from '@tensorflow/tfjs-backend-webgl/dist/index.js';
+export * from '@tensorflow/tfjs-backend-wasm/dist/index.js';
+
+// add webgpu to bundle, experimental
+export * from '@tensorflow/tfjs-backend-webgpu/dist/index.js';
+
+// export versions, overrides version object from @tensorflow/tfjs
+export { version } from '../dist/tfjs.version.js';
+
+// export utility types used by Human
+export { Tensor } from '@tensorflow/tfjs/dist/index.js';
+export { GraphModel } from '@tensorflow/tfjs-converter/dist/index';

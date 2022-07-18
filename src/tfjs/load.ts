85656cdef50c9fe379fcebcbc7c24d6370b1d6fa
Vladimir Mandic <mandic00@live.com>
2022-07-17 21:31:08 -0400

    beta for upcoming major release

--- src/tfjs/load.ts
+++ src/tfjs/load.ts
@@ -2,6 +2,7 @@ import { log, join } from '../util/util';
 import * as tf from '../../dist/tfjs.esm.js';
 import type { GraphModel } from './types';
 import type { Config } from '../config';
+import * as modelsDefs from '../../models/models.json';
 
 const options = {
   cacheModels: true,
@@ -11,14 +12,15 @@ const options = {
   modelBasePath: '',
 };
 
-type ModelStats = {
+export type ModelInfo = {
   name: string,
-  cached: boolean,
-  manifest: number,
-  weights: number,
+  inCache: boolean,
+  sizeDesired: number,
+  sizeFromManifest: number,
+  sizeLoadedWeights: number,
 }
 
-export const modelStats: Record<string, ModelStats> = {};
+export const modelStats: Record<string, ModelInfo> = {};
 
 async function httpHandler(url, init?): Promise<Response | null> {
   if (options.debug) log('load model fetch:', url, init);
@@ -34,14 +36,15 @@ export function setModelLoadOptions(config: Config) {
 export async function loadModel(modelPath: string | undefined): Promise<GraphModel> {
   let modelUrl = join(options.modelBasePath, modelPath || '');
   if (!modelUrl.toLowerCase().endsWith('.json')) modelUrl += '.json';
-  const modelPathSegments = modelUrl.split('/');
+  const modelPathSegments = modelUrl.includes('/') ? modelUrl.split('/') : modelUrl.split('\\');
   const shortModelName = modelPathSegments[modelPathSegments.length - 1].replace('.json', '');
   const cachedModelName = 'indexeddb://' + shortModelName; // generate short model name for cache
   modelStats[shortModelName] = {
     name: shortModelName,
-    manifest: 0,
-    weights: 0,
-    cached: false,
+    sizeFromManifest: 0,
+    sizeLoadedWeights: 0,
+    sizeDesired: modelsDefs[shortModelName],
+    inCache: false,
   };
   options.cacheSupported = (typeof window !== 'undefined') && (typeof window.localStorage !== 'undefined') && (typeof window.indexedDB !== 'undefined'); // check if running in browser and if indexedb is available
   let cachedModels = {};
@@ -50,9 +53,9 @@ export async function loadModel(modelPath: string | undefined): Promise<GraphMod
   } catch {
     options.cacheSupported = false;
   }
-  modelStats[shortModelName].cached = (options.cacheSupported && options.cacheModels) && Object.keys(cachedModels).includes(cachedModelName); // is model found in cache
+  modelStats[shortModelName].inCache = (options.cacheSupported && options.cacheModels) && Object.keys(cachedModels).includes(cachedModelName); // is model found in cache
   const tfLoadOptions = typeof fetch === 'undefined' ? {} : { fetchFunc: (url, init?) => httpHandler(url, init) };
-  const model: GraphModel = new tf.GraphModel(modelStats[shortModelName].cached ? cachedModelName : modelUrl, tfLoadOptions) as unknown as GraphModel; // create model prototype and decide if load from cache or from original modelurl
+  const model: GraphModel = new tf.GraphModel(modelStats[shortModelName].inCache ? cachedModelName : modelUrl, tfLoadOptions) as unknown as GraphModel; // create model prototype and decide if load from cache or from original modelurl
   let loaded = false;
   try {
     // @ts-ignore private function
@@ -60,16 +63,16 @@ export async function loadModel(modelPath: string | undefined): Promise<GraphMod
     if (options.debug) log('model load handler:', model['handler']);
     // @ts-ignore private property
     const artifacts = await model.handler.load(); // load manifest
-    modelStats[shortModelName].manifest = artifacts?.weightData?.byteLength || 0;
+    modelStats[shortModelName].sizeFromManifest = artifacts?.weightData?.byteLength || 0;
     model.loadSync(artifacts); // load weights
     // @ts-ignore private property
-    modelStats[shortModelName].weights = model?.artifacts?.weightData?.byteLength || 0;
-    if (options.verbose) log('load model:', model['modelUrl'], { bytes: modelStats[shortModelName].weights }, options);
+    modelStats[shortModelName].sizeLoadedWeights = model?.artifacts?.weightData?.byteLength || 0;
+    if (options.verbose) log('load model:', model['modelUrl'], { bytes: modelStats[shortModelName].sizeLoadedWeights }, options);
     loaded = true;
   } catch (err) {
     log('error loading model:', modelUrl, err);
   }
-  if (loaded && options.cacheModels && options.cacheSupported && !modelStats[shortModelName].cached) { // save model to cache
+  if (loaded && options.cacheModels && options.cacheSupported && !modelStats[shortModelName].inCache) { // save model to cache
     try {
       const saveResult = await model.save(cachedModelName);
       log('model saved:', cachedModelName, saveResult);
